ba7815e57c7af6941c017f3b01f34b60202be2d7
Germain LEFEBVRE <germain.lefebvre4@gmail.com>
2021-01-15 14:42:00 +0100

    feat(module): Use remote module with variables

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
 module "dummy" {
-    source = "git@github.com:ineat/blog-terraform-module-dummy.git"
+    #source = "git@github.com:ineat/blog-terraform-module-dummy.git?ref=module-basic-variables"
+    source = "../blog-terraform-module-dummy/"
+
+    directory = "/tmp"
 }
