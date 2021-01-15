23b7c01175b2eb98874ca245e26b95f01ea40253
Germain LEFEBVRE <germain.lefebvre4@gmail.com>
2021-01-15 15:30:25 +0100

    feat(module): Use remote module with depends_on

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+module "dummy-depending-on-dummy" {
+    source = "git@github.com:ineat/blog-terraform-module-dummy.git?ref=module-basic"
+
+    depends_on = [module.dummy]
+}
+
 module "dummy" {
     source = "git@github.com:ineat/blog-terraform-module-dummy.git"
 }
